32d1ac47d98e16895ae9366ee645c7f9d9de348d
k60yama <k60yama@gmail.com>
2012-09-25 15:03:24 +0900

    befer class0925

--- src/sample/application/fingerpaint/FingerPaintActivity.java
+++ src/sample/application/fingerpaint/FingerPaintActivity.java
@@ -1,14 +1,24 @@
 package sample.application.fingerpaint;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.DecimalFormat;
+
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Display;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
@@ -92,5 +102,83 @@ public class FingerPaintActivity extends Activity implements OnTouchListener{
 		canvas.drawColor(Color.WHITE);
 		iv.setImageBitmap(bitmap);
 		iv.setOnTouchListener(this);
+	}
+	
+	//保存メイン処理
+	public void save(){
+		SharedPreferences prefs = this.getSharedPreferences(
+				"FingerPaintPreferences", MODE_PRIVATE);
+		Integer imageNumber = prefs.getInt("imageNumber", 1);
+		File file = null;
+		
+		//SDカードチェック
+		if(externalMediaChecker()){
+			DecimalFormat form = new DecimalFormat("0000");
+			String path = Environment.getExternalStorageDirectory() + "/mypaint/";
+			File outDir = new File(path);
+			
+			//フォルダ作成
+			if(!outDir.exists()){
+				outDir.mkdir();
+			}
+			
+			//ファイル作成(ファイルが存在するまで)
+			do{
+				file = new File(path + "img" + form.format(imageNumber) + ".png");
+				imageNumber++;
+			}while(file.exists());
+			
+			//連番保存
+			if(writeImage(file)){
+				SharedPreferences.Editor editor = prefs.edit();
+				editor.putInt("imageNumber", imageNumber);
+				editor.commit();
+			}
+		}
+	}
+	
+	//microSDカードの有無確認
+	public boolean externalMediaChecker(){
+		
+		boolean result = false;
+		String status = Environment.getExternalStorageState();
+		
+		if(status.equals(Environment.MEDIA_MOUNTED)){
+			result = true;
+		}
+		return result;
+	}
+	
+	//書き込み処理
+	public boolean writeImage(File file){
+		try{
+			FileOutputStream fo = new FileOutputStream(file);
+			bitmap.compress(CompressFormat.PNG, 100, fo);
+			fo.flush();
+			fo.close();
+		}catch(Exception e){
+			System.out.println(e.getLocalizedMessage());
+			return false;
+		}
+		return true;
+	}
+	
+	//メニュー生成
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu){
+		MenuInflater mi = this.getMenuInflater();
+		mi.inflate(R.menu.menu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	//メニュー選択処理
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+		switch(item.getItemId()){
+		case R.id.menu_save:
+			save();
+			break;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 }
